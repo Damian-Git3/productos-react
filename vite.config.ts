6bb0df1550add8b16ac286553954d6d4ef96434f
Damian-Git3 <21002103@alumnos.utleon.edu.mx>
2024-11-09 20:15:00 -0600

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        sourcemap: true,
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      includeAssets: ["vite.svg"],
+      manifest: {
+        name: "Productos React",
+        short_name: "RC Productos",
+        description: "Productos React",
+        theme_color: "#ffffff",
+      },
+    }),
+  ],
+});
